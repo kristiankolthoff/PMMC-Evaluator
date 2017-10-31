8fe8c03db39d2babcad2071b7a91d5e0959df943
kristiankolthoff <k_kolthoff@web.de>
2017-10-31 12:23:35 +0100

    Sorting metric added

---
+++ src/main/java/de/unima/ki/pmmc/evaluator/metrics/sorting/MetricSort.java
@@ -0,0 +1,49 @@
+package de.unima.ki.pmmc.evaluator.metrics.sorting;
+
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+
+import de.unima.ki.pmmc.evaluator.data.Report;
+import de.unima.ki.pmmc.evaluator.generator.MetricBinding;
+import de.unima.ki.pmmc.evaluator.generator.MetricGroupBinding;
+import de.unima.ki.pmmc.evaluator.metrics.Metric;
+
+public class MetricSort implements Comparator<Report> {
+
+	private Metric metric;
+	private boolean desc;
+	
+	public MetricSort(Metric metric, boolean desc) {
+		this.metric = metric;
+		this.desc = desc;
+	}
+
+	@Override
+	public int compare(Report report1, Report report2) {
+		double metricValue1 = getMetricValue(report1, metric);
+		double metricValue2 = getMetricValue(report2, metric);
+		double diff = metricValue1 - metricValue2;
+		if(diff < 0) {
+			if(desc) return 1;
+			else return -1;
+		}
+		else if(diff > 0) {
+			if(desc) return -1;
+			else return 1;
+		}
+		else return 0;
+	}
+	
+	private double getMetricValue(Report report, Metric metric) {
+		for(MetricGroupBinding metricGroupBinding : report.getBindings()) {
+			for(MetricBinding metricBinding : metricGroupBinding) {
+				if(metricBinding.getMetric().equals(metric)) {
+					return metricBinding.getValue();
+				}
+			}
+		}
+		throw new NoSuchElementException("The metric " + metric.getName() + 
+				" was not found in the configuration");
+	}
+
+}
