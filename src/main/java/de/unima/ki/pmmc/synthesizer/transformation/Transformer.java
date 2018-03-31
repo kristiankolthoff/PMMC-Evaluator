b9d59ecdfaed3848f4aa132b840b556caa6d43f5
kristiankolthoff <k_kolthoff@web.de>
2018-03-31 18:11:58 +0200

    New Transformer interface with proper return types for gs

--- src/main/java/de/unima/ki/pmmc/synthesizer/transformation/Transformer.java
+++ src/main/java/de/unima/ki/pmmc/synthesizer/transformation/Transformer.java
@@ -1,7 +1,6 @@
 package de.unima.ki.pmmc.synthesizer.transformation;
 
 import java.io.File;
-import java.util.Collection;
 import java.util.List;
 
 import org.apache.commons.lang3.tuple.Pair;
@@ -11,23 +10,25 @@ import de.unima.ki.pmmc.evaluator.model.Activity;
 
 public interface Transformer {
 	
-	public Alignment initializeModel(String modelPath);
+	public Alignment initializeModel(String modelPath, String modelName);
 	
-	public void transformActivity(String id, Activity activity);
+	public Activity transformActivity(String id, String transformation, boolean isValidCorres);
+	
+	public Activity transformActivity(Activity activity, boolean isValidCorres);
 
-	public Pair<Activity, List<Activity>> one2manyParallel(String oriActivity, String... replacements);
+	public List<Pair<Activity, Activity>> one2manyParallel(String oriActivity, String... replacements);
 	
-	public Pair<Activity, List<Activity>> one2manySequential(String oriActivity, String... replacements);
+	public List<Pair<Activity, Activity>> one2manySequential(String oriActivity, String... replacements);
 	
-	public Pair<Activity, List<Activity>> many2oneParallel(String newActivity, String... replacements);
+	public List<Pair<Activity, Activity>> many2oneParallel(String newActivity, String... replacements);
 	
-	public Pair<Activity, List<Activity>> many2oneSequential(String newActivity, String... replacements);
+	public List<Pair<Activity, Activity>> many2oneSequential(String newActivity, String... replacements);
 	
-	public void addIrrelevant(Activity... activities);
+	public void addIrrelevant(AddStrategy addStrategy, Activity... activities);
 	
-	public void addIrrelevantFromDataset(File dataset, double ratio);
+	public void addIrrelevantFromDataset(File dataset, double ratio, AddStrategy addStrategy);
 	
-	public void flip(Direction direction);
+	public List<Pair<Activity, Activity>> flip(Direction direction);
 	
 	public void replaceSynonyms(double probability, List<String> synonyms);
 	
@@ -36,5 +37,9 @@ public interface Transformer {
 	public void partMappingFromDataset(File dataset);
 	
 	public boolean writeModel(String name, String path);
+	
+	public List<Activity> getActivitiesByName(String name);
+	
+	public Activity getActivityById(String id);
 	
 }
